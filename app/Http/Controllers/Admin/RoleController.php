92dfb3cd48d7eb4187610eac3ca7d5f812b641de
dave <1316896128@qq.com>
2020-05-01 10:06:11 +0800

    资源控制器，资源路由

---
+++ app/Http/Controllers/Admin/RoleController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class RoleController extends BaseController
+{
+    /**
+     * Display a listing of the resource.
+     * 列表
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     * 显示详情
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     * 修改页面
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     * 修改操作
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- routes/admin/admin.php
+++ routes/admin/admin.php
@@ -1,33 +1,35 @@
 <?php
 
-Route::group(['prefix' => 'admin', 'namespace' => 'Admin'], function () {
+Route::group(['prefix' => 'admin', 'namespace' => 'Admin', 'as' => 'admin.'], function () {
     // 登录先生
-    Route::get('login', 'LoginController@index')->name('admin.login');
+    Route::get('login', 'LoginController@index')->name('login');
     // 登录处理
-    Route::post('login', 'LoginController@login')->name('admin.login');
+    Route::post('login', 'LoginController@login')->name('login');
     // 欢迎页面,路由中间件
-//    Route::get('welcome', 'IndexController@welcome')->name('admin.welcome')->middleware(['ckadmin']);
+//    Route::get('welcome', 'IndexController@welcome')->name('welcome')->middleware(['ckadmin']);
     // 分组中间件
     Route::group(['middleware' => ['ckadmin']], function () {
         // 后台首页
-        Route::get('index', 'IndexController@index')->name('admin.index');
-        Route::get('welcome', 'IndexController@welcome')->name('admin.welcome');
+        Route::get('index', 'IndexController@index')->name('index');
+        Route::get('welcome', 'IndexController@welcome')->name('welcome');
         // 退出登录
-        Route::get('logout', 'IndexController@logout')->name('admin.logout');
+        Route::get('logout', 'IndexController@logout')->name('logout');
         // 管理员列表
-        Route::get('user/index', 'UserController@index')->name('admin.user.index');
+        Route::get('user/index', 'UserController@index')->name('user.index');
         // 添加管理员
-        Route::get('user/add', 'UserController@create')->name('admin.user.add');
-        Route::post('user/add', 'UserController@store')->name('admin.user.add');
+        Route::get('user/add', 'UserController@create')->name('user.add');
+        Route::post('user/add', 'UserController@store')->name('user.add');
         // 删除管理员用户
-        Route::delete('user/del/{id}', 'UserController@del')->name('admin.user.del');
+        Route::delete('user/del/{id}', 'UserController@del')->name('user.del');
         // 批量删除管理员
-        Route::delete('user/delall', 'UserController@delall')->name('admin.user.delall');
+        Route::delete('user/delall', 'UserController@delall')->name('user.delall');
         // 还原管理员
-        Route::get('user/restore/{id}', 'UserController@restore')->name('admin.user.restore');
+        Route::get('user/restore/{id}', 'UserController@restore')->name('user.restore');
         // 修改管理员
-        Route::get('user/edit/{id}','UserController@edit')->name('admin.user.edit');
-        Route::put('user/edit/{id}','UserController@update')->name('admin.user.edit');
+        Route::get('user/edit/{id}', 'UserController@edit')->name('user.edit');
+        Route::put('user/edit/{id}', 'UserController@update')->name('user.edit');
+        // 权限管理，定义资源路由,as 用来设置别名前缀
+        Route::resource('role', 'RoleController');
     });
 });
 Route::get('email', function () {
