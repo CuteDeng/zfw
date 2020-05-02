385b69b3763e0f92562a495a4e9f62cad9f0acc3
dave <1316896128@qq.com>
2020-05-02 17:51:44 +0800

    文章内容使用ueditor

--- app/Http/Controllers/Admin/ArticleController.php
+++ app/Http/Controllers/Admin/ArticleController.php
@@ -24,8 +24,8 @@ class ArticleController extends Controller
             $query = Article::where('id', '>', 0);
             // 根据日期过滤
             if (!empty($datemin) && !empty($datemax)) {
-                $datemin = date('Y-m-d H:i:s', strtotime($datemin. ' 00:00:00'));
-                $datemax = date('Y-m-d H:i:s', strtotime($datemax. ' 23:59:59'));
+                $datemin = date('Y-m-d H:i:s', strtotime($datemin . ' 00:00:00'));
+                $datemax = date('Y-m-d H:i:s', strtotime($datemax . ' 23:59:59'));
                 $query->whereBetween('created_at', [$datemin, $datemax]);
             }
             // 根据关键字过滤
@@ -50,12 +50,12 @@ class ArticleController extends Controller
 
     /**
      * Show the form for creating a new resource.
-     *
+     * 添加文章
      * @return \Illuminate\Http\Response
      */
     public function create()
     {
-        //
+        return view('admin.article.create');
     }
 
     /**
