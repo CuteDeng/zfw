ff0885e82a6cdf359b15f63b8f530af0afbbcdd6
dave <1316896128@qq.com>
2020-05-02 10:42:45 +0800

    文章表创建，数据填充

---
+++ database/migrations/2020_05_02_102419_create_articles_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     * 文章表
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title', 200)->comment('标题');
+            $table->string('desc', 255)->default('')->comment('摘要');
+            $table->string('pic', 200)->default('')->comment('封面');
+            $table->text('body')->comment('文章内容');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
