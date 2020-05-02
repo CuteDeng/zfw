0de140a6d64585dff55dddc87b5898582b82e4ce
dave <1316896128@qq.com>
2020-05-02 15:38:30 +0800

    使用给模型增加字段并设置访问器的方式显示按钮

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Article extends Base
 {
-    //
+    // 给模型追加一个数据库不存在的字段
+    protected $appends = ['action'];
+    // 给该字段设置访问器
+    public function getActionAttribute()
+    {
+        return $this->editBtn('admin.article.edit') .'&nbsp;'. $this->delBtn('admin.article.destroy');
+    }
 }
